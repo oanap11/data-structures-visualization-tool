761035c55e058b14e2217902db0b0ab901565652
Oana <oana.apvel@gmail.com>
2023-09-08 19:12:53 +0300

    Refactor Main class to reduce code duplication

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -7,7 +7,6 @@ import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -17,7 +16,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -28,156 +26,105 @@ import dsa.panels.StackPanel;
 import dsa.panels.TreePanel;
 
 public class Main extends JFrame {
+	
+	private static final int PANEL_WIDTH = 800;
+    private static final int PANEL_HEIGHT = 800;
+    private static final String IMAGE_PATH = "/images/idk.png";
+    private static final Color MENU_BAR_BACKGROUND_COLOR = new Color(255, 87, 51);
 
+    static JPanel mainPanel;
+    static JPanel welcomePanel;
     static ListPanel listPanel;
     static PathFindingPanel algoPanel;
     static StackPanel stackPanel;
     static QueuePanel queuePanel;
     static TreePanel treePanel;
-    static JPanel mainPanel;
-    static JPanel welcomePanel;
-    static JPanel avlPanel;
-
-    private JMenuItem arrayMenu;
+    
     private JMenu meniuPrincipal;
-    private JMenu jMenu2;
-    private JMenuBar jMenuBar1;
-    private JSlider jSlider1;
-    private JMenuItem linkedListMenu;
-    private JMenuItem queueMenu;
-    private JMenuItem stackMenu;
-    private JMenuItem treeMenu;
-    private JMenuItem algoMenu;
+    private JMenuBar menuBar;
 
     ImageIcon stackIcon;
 
     public Main() {
-    	logIn();
 
-        initComponents();
-        welcomePanel = new JPanel();
+        initComponents();  
+        initializePanels();   
+        setWelcomePicture();
+        setMainPanel();
+	    addAllPanelsToMain();
+        hidePanels();
+        this.pack();
+    }
+    
+    private void setMainPanel() {
+    	mainPanel.setSize(PANEL_WIDTH, PANEL_HEIGHT);
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+    }
+    
+    private void setWelcomePicture() {
+    	JLabel welcomePicture = new JLabel();
+    	welcomePicture.setIcon(new ImageIcon(getClass().getResource(IMAGE_PATH)));
+        welcomePanel.add(welcomePicture);
+    }
+    
+    private void initializePanels() {
+    	welcomePanel = new JPanel();
         mainPanel =  new JPanel();
         listPanel = new ListPanel();
         stackPanel = new StackPanel();
         queuePanel = new QueuePanel();
         treePanel = new TreePanel();
         algoPanel = new PathFindingPanel();
-
-        JLabel picLabel = new JLabel();
-        picLabel.setIcon(new ImageIcon(getClass().getResource("/images/idk.png")));
-        welcomePanel.add(picLabel);
-
-        mainPanel.setSize(800, 800);
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
-
-	    welcomePanel.setBackground(Color.white);
-	    mainPanel.add(welcomePanel);
-
+    }
+    
+    private void addAllPanelsToMain() {
+    	mainPanel.add(welcomePanel);
         mainPanel.add(listPanel);
         mainPanel.add(stackPanel);
         mainPanel.add(treePanel);
         mainPanel.add(queuePanel);
         mainPanel.add(algoPanel);
-
         add(mainPanel, BorderLayout.CENTER);
-        hidePanels();
-        this.pack();
     }
-
-    private void logIn() {
-		//login.setVisible(true);
-	}
 
     @SuppressWarnings("unchecked")
     private void initComponents() {
 
-        jSlider1 = new JSlider();
-        jMenuBar1 = new JMenuBar();
+    	menuBar = new JMenuBar();
         meniuPrincipal = new JMenu();
-
-        linkedListMenu = new JMenuItem();
-        treeMenu = new JMenuItem();
-        stackMenu = new JMenuItem();
-        queueMenu = new JMenuItem();
-        arrayMenu = new JMenuItem();
-        algoMenu = new JMenuItem();
-        jMenu2 = new JMenu();
 
         stackIcon = new ImageIcon("/images/back.png");
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("Aplicatie pentru vizualizarea structurilor de date");
-        setSize(new Dimension(800, 800));
-        setLocationRelativeTo(null);
-        setResizable(false);
+        initUI();
 
         meniuPrincipal.setText("Structuri de date \n" + "▼\n" + "");
 
-        linkedListMenu.setText("Liste Inlantuite");
-        linkedListMenu.addActionListener(new ActionListener() {
-            @Override
-			public void actionPerformed(ActionEvent evt) {
-                linkedListMenuActionPerformed(evt);
-            }
-        });
-        meniuPrincipal.add(linkedListMenu);
+        addDataStructureMenuItem("Liste Inlantuite", this::linkedListMenuActionPerformed);
+        addDataStructureMenuItem("Arbori", this::treeMenuActionPerformed);
+        addDataStructureMenuItem("Stiva", this::stackMenuActionPerformed);
+        addDataStructureMenuItem("Coada", this::queueMenuActionPerformed);
+        addDataStructureMenuItem("Algoritmi de cautare", this::algoMenuActionPerformed);
 
-        treeMenu.setText("Arbori");
-        treeMenu.addActionListener(new ActionListener() {
-            @Override
-			public void actionPerformed(ActionEvent evt) {
-                treeMenuActionPerformed(evt);
-            }
-        });
-        meniuPrincipal.add(treeMenu);
-
-        stackMenu.setText("Stiva");
-        stackMenu.addActionListener(new ActionListener() {
-            @Override
-			public void actionPerformed(ActionEvent evt) {
-                stackMenuActionPerformed(evt);
-            }
-        });
-        meniuPrincipal.add(stackMenu);
-
-        queueMenu.setText("Coada");
-        queueMenu.addActionListener(new ActionListener() {
-            @Override
-			public void actionPerformed(ActionEvent evt) {
-                queueMenuActionPerformed(evt);
-            }
-        });
-        meniuPrincipal.add(queueMenu);
-
-        algoMenu.setText("Algoritmi de cautare");
-        algoMenu.addActionListener(new ActionListener() {
-            @Override
-			public void actionPerformed(ActionEvent evt) {
-                algoMenuActionPerformed(evt);
-            }
-        });
-        meniuPrincipal.add(algoMenu);
-
-        jMenuBar1.setPreferredSize(new Dimension(100, 100));
-        jMenuBar1.add(meniuPrincipal);
-        jMenuBar1.setLayout(new GridBagLayout());
-        setJMenuBar(jMenuBar1);
+        menuBar.setPreferredSize(new Dimension(100, 100));
+        menuBar.add(meniuPrincipal);
+        menuBar.setLayout(new GridBagLayout());
+        setJMenuBar(menuBar);
 
         pack();
     }
-
-
-    void setPanelSize() {
-    	int w = 800;
-    	int h = 800;
-        mainPanel.setSize(w, h);
-
-        listPanel.setSize(w,h);
-        stackPanel.setSize(w,h);
-        queuePanel.setSize(w,h);
-        treePanel.setSize(w,h);
-
-        System.out.println("Width = " + w + "\nHeight = " + h);
+    
+    private void addDataStructureMenuItem(String text, ActionListener actionListener) {
+        JMenuItem menuItem = new JMenuItem(text);
+        menuItem.addActionListener(actionListener);
+        meniuPrincipal.add(menuItem);
+    }
+    
+    void initUI() {
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Aplicatie pentru vizualizarea structurilor de date");
+        setSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
 
     void hidePanels() {
@@ -187,50 +134,35 @@ public class Main extends JFrame {
         treePanel.setVisible(false);
         algoPanel.setVisible(false);
     }
+    
+    private void switchToPanel(JPanel panel) {
+        hidePanels();
+        panel.setVisible(true);
+        welcomePanel.setVisible(false);
+    }
+
+    private void menuItemActionPerformed(ActionEvent evt, JPanel panel) {
+        switchToPanel(panel);
+    }
 
     private void linkedListMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        listPanel.setVisible(true);
-        welcomePanel.setVisible(false);
+        menuItemActionPerformed(evt, listPanel);
     }
 
     private void treeMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        treePanel.setVisible(true);
-        welcomePanel.setVisible(false);
-    }
-
-    private void avlMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        avlPanel.setVisible(true);
-        welcomePanel.setVisible(false);
+        menuItemActionPerformed(evt, treePanel);
     }
 
     private void stackMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        stackPanel.setVisible(true);
-        welcomePanel.setVisible(false);
+        menuItemActionPerformed(evt, stackPanel);
     }
 
     private void queueMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        queuePanel.setVisible(true);
-        welcomePanel.setVisible(false);
+        menuItemActionPerformed(evt, queuePanel);
     }
 
     private void algoMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-        algoPanel.setVisible(true);
-        welcomePanel.setVisible(false);
-    }
-
-    private void arrayMenuActionPerformed(ActionEvent evt) {
-        hidePanels();
-    }
-
-    private void formWindowStateChanged(WindowEvent evt) {
-        this.revalidate();
-        this.repaint();
+        menuItemActionPerformed(evt, algoPanel);
     }
 
     public static void main(String args[]) {
@@ -246,13 +178,12 @@ public class Main extends JFrame {
     	UIManager.put("Menu.foreground", Color.WHITE);
     	UIManager.put("Menu.font", f);
     	UIManager.put("MenuItem.font", f);
-    	UIManager.put("MenuBar.background", new Color(255, 87, 51));
-        UIManager.put("MenuItem.background", new Color(255, 87, 51));
+    	UIManager.put("MenuBar.background", MENU_BAR_BACKGROUND_COLOR);
+        UIManager.put("MenuItem.background", MENU_BAR_BACKGROUND_COLOR);
 
 		SwingUtilities.invokeLater(new Runnable(){
 			@Override
 			public void run() {
-				//new LoginForm().setVisible(true);
 				new Main().setVisible(true);
 			}
 		});
